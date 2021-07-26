92ab0d384c27af214587981268b3842a86500b6f
Simone Machado <simone.p.machado@accenture.com>
2021-07-26 04:52:38 -0300

    add geral modules / add component header

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSliderModule } from '@angular/material/slider';
+//import { MatSliderModule } from '@angular/material/slider';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/template/header/header.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    MatSliderModule,
-    BrowserAnimationsModule    
+//    MatSliderModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,    
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
