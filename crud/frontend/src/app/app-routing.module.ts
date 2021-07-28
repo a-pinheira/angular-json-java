6adda346dcf6126908a4ca2f1b73ffb5d8da1483
Simone Machado <simone.p.machado@accenture.com>
2021-07-27 23:11:50 -0300

    Up / Navegando entre Components

--- crud/frontend/src/app/app-routing.module.ts
+++ crud/frontend/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
+/*Primeiro Objeto: array do tipo Routes*/
+const routes: Routes = [
+  {
+  /**VAntagens do Typescript
+   * Elementos tipados se consegue ter o Code Complete
+   * Propriedade do Objetos */
 
+  path: "",
+  /**Primeira Rota */
+  component: HomeComponent
+  },
+  {
+    path: "products",
+    /**Segunda Rota - Componente de Cadastro de Produto*/
+    component: ProductCrudComponent
+  }
+];
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from  '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { MatCardModule } from '@angular/material/card';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,6 +22,7 @@ import { MatCardModule } from '@angular/material/card';
     FooterComponent,
     NavComponent,
     HomeComponent,
+    ProductCrudComponent,
   ],
   imports: [
     BrowserModule,
